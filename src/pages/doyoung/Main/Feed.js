db81a31a9c633e37bbb3a3f7c242698cac05e342
김도영 <go3152@gmail.com>
2022-10-08 18:17:51 +0900

    ADD : 메인페이지 피드 완성(댓글추가 기능 제외)

--- src/pages/doyoung/Main/Feed.js
+++ src/pages/doyoung/Main/Feed.js
@@ -6,6 +6,7 @@ import whiteHeart from '../../../assets/doyoung/whiteHeart.png';
 import commentIcon from '../../../assets/doyoung/commentIcon.png';
 import dm from '../../../assets/doyoung/dm.png';
 import saveIcon from '../../../assets/doyoung/saveIcon.png';
+import emoji from '../../../assets/doyoung/emoji.png';
 
 function Feed() {
   function IdBox() {
@@ -51,10 +52,38 @@ function Feed() {
     );
   }
 
+  function DiscribeBox() {
+    return (
+      <div className="discribeBox">
+        <span className="discribeBoxId">_rianess </span>
+        <span> 고양이사진~~!!!!!!!!!!! </span>
+      </div>
+    );
+  }
+
   function NumberOfLike() {
     return (
       <div className="NumberOfLike">
         kimdoyoung12345님 외 4,325,721명이 좋아합니다.
+      </div>
+    );
+  }
+
+  function PostedOneDayAgo() {
+    return (
+      <div className="postedOneDayAgo">
+        <span className="left">1일전</span>
+        <span className="right">번역보기</span>
+      </div>
+    );
+  }
+
+  function InputAndButton() {
+    return (
+      <div className="InputAndButton">
+        <img src={emoji} alt={emoji} className="emoji" />
+        <input className="commentInput" />
+        <button className="commentButton">게시</button>
       </div>
     );
   }
@@ -64,13 +93,10 @@ function Feed() {
       <PhotoBox />
       <LikeBox />
       <NumberOfLike />
-      <div className="discribeBox">
-        <span className="discribeBoxId">_rianess </span>
-        <span> 고양이사진~~!!!!!!!!!!! </span>
-      </div>
-      <div className="CommentBox">컴포넌트할 댓글박스@@@</div>
-      <div className="postedOneDayAgo">1일전 번역보기</div>
-      <div className="InputAndButton">이모지 댓글인풋 게시버튼</div>
+      <DiscribeBox />
+      <div className="CommentBox">kimdoyoung12345 컴포넌트할 댓글박스@@@</div>
+      <PostedOneDayAgo />
+      <InputAndButton />
     </div>
   );
 }
