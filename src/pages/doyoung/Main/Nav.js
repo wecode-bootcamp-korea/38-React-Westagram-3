0c59fb794445e36fddd7d6f3688c0fb52a8d7cc7
김도영 <go3152@gmail.com>
2022-10-08 15:05:47 +0900

    ADD : 메인페이지 Nav 레이아웃

--- src/pages/doyoung/Main/Nav.js
+++ src/pages/doyoung/Main/Nav.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import home from '../../../assets/doyoung/home.png';
+import dm from '../../../assets/doyoung/dm.png';
+import plus from '../../../assets/doyoung/plus.png';
+import compass from '../../../assets/doyoung/compass.png';
+import whiteHeart from '../../../assets/doyoung/whiteHeart.png';
+import mypic from '../../../assets/doyoung/mypic.png';
 function Nav() {
   return (
     <div className="Nav">
-      도영 네비게이션 바 입니다 위에를 띄운 이유는 Team Nav 자리..
+      <div className="NavLeft">Westagram</div>
+      <input className="NavInput" />
+      <div className="NavRight">
+        <img src={home} alt="home" className="NavRightImg" />
+        <img src={dm} alt="dm" className="NavRightImg" />
+        <img src={plus} alt="plus" className="NavRightImg" />
+        <img src={compass} alt="compass" className="NavRightImg" />
+        <img src={whiteHeart} alt="whiteHeart" className="NavRightImg" />
+        <img src={mypic} alt="mypic" className="NavRightImg" />
+      </div>
     </div>
   );
 }
