40b421bc765bab8c6dc604096086170ff1413760
김도영 <go3152@gmail.com>
2022-10-08 17:25:55 +0900

    ADD : 메인 피드 LikeBox

--- src/pages/doyoung/Main/Feed.js
+++ src/pages/doyoung/Main/Feed.js
@@ -2,6 +2,10 @@ import React from 'react';
 import rian from '../../../assets/doyoung/rian.png';
 import dot3 from '../../../assets/doyoung/dot3.png';
 import cat from '../../../assets/doyoung/cat.png';
+import whiteHeart from '../../../assets/doyoung/whiteHeart.png';
+import commentIcon from '../../../assets/doyoung/commentIcon.png';
+import dm from '../../../assets/doyoung/dm.png';
+import saveIcon from '../../../assets/doyoung/saveIcon.png';
 
 function Feed() {
   function IdBox() {
@@ -23,11 +27,34 @@ function Feed() {
       </div>
     );
   }
+
+  function LikeBox() {
+    return (
+      <div className="LikeBox">
+        <div>
+          <img
+            src={whiteHeart}
+            className="whiteHeart feedIcon"
+            alt={whiteHeart}
+          />
+          <img
+            src={commentIcon}
+            className="commentIcon feedIcon"
+            alt={commentIcon}
+          />
+          <img src={dm} className="dm feedIcon" alt={dm} />
+        </div>
+        <div>
+          <img src={saveIcon} className="saveIcon feedIcon" alt={saveIcon} />
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="Feed">
       <IdBox />
       <PhotoBox />
-      <div className="LikeBox">좋아요 댓글 공유 저장</div>
+      <LikeBox />
       <div className="NumberOfLike">~명이 좋아합니다</div>
       <div className="discribeBox">게시자 아이디 내용</div>
       <div className="CommentBox">컴포넌트할 댓글박스@@@</div>
