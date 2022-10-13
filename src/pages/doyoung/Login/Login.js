b499344b5b27647299540cb38e1fad6cee745bd6
김도영 <go3152@gmail.com>
2022-10-13 18:15:39 +0900

    Modify : 로그인페이지 버튼 -> 로그인회원가입 실습용으로 수정함-> 회원가입버튼 추가함. and  로그인버튼클릭시 메인으로 가는거 라우터 없애고 fetch함수 동작하게 만듬

--- src/pages/doyoung/Login/Login.js
+++ src/pages/doyoung/Login/Login.js
@@ -26,7 +26,7 @@ function Login() {
     } else {
       setButtonToggleState(true);
     }
-    console.log(ValueOfIdInput);
+    // console.log(ValueOfIdInput);
   }, [ValueOfIdInput, ValueOfPwInput]);
 
   //버튼누르면 메인으로 인도할 네비게이터 생성
@@ -34,24 +34,83 @@ function Login() {
   const goToMain = () => {
     navigate('/doyoung');
   };
+  ////////////////////////////////////////////////////////////////로그인버튼 온클릭함수
+  function loginButtonOnClickFunction(event) {
+    event.preventDefault();
+    console.log('로그인 온클릭 !!');
+    fetch('http://10.58.52.139:3000/auth/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({
+        email: ValueOfIdInput,
+        name: 'doyoung',
+        password: ValueOfPwInput,
+        profile_image: 'fdfsaf',
+      }),
+    })
+      .then(response => {
+        if (response.ok === true) {
+          return response.json();
+        }
+        throw new Error('에러 발생!');
+      })
+      .catch(error => console.log(error))
+      .then(data => {
+        localStorage.setItem('TOKEN', data.accessToken);
+      });
+  }
 
+  ////////////////////////////////////////////////////////////////회원가입버튼 온클릭함수
+  function signUpButtonOnClickFunction(event) {
+    event.preventDefault();
+    console.log('회원가입!!');
+    fetch('http://10.58.52.139:3000/auth/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({
+        email: ValueOfIdInput,
+        // name: 'doyoung',
+        password: ValueOfPwInput,
+        // profile_image: 'fdfsaf',
+      }),
+    })
+      .then(response => {
+        if (response.ok === true) {
+          return response.json();
+        }
+        throw new Error('에러 발생!');
+      })
+      .catch(error => console.log(error));
+  }
   const [buttonToggleState, setButtonToggleState] = useState(true);
   //이후 로그인 버튼에 온클릭함수 넣었음. 누르면 메인으로감 (../Main/Main 이런거 아니고 /doyoung임)
 
-  //리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴
+  //리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴리턴
   return (
     <div className="Login">
       <div className="contentsBox">
         <div className="Logo">Westagram</div>
-        <input className="Input" onChange={MakeIdInputToState} />
-        <input className="Input" onChange={MakePwInputToState} />
-        <button
-          className="LoginButton"
-          onClick={goToMain}
-          disabled={buttonToggleState}
-        >
-          로그인
-        </button>
+        <form className="formFrame">
+          <input className="Input" onChange={MakeIdInputToState} />
+          <input className="Input" onChange={MakePwInputToState} />
+          <button
+            className="LoginButton"
+            onClick={loginButtonOnClickFunction}
+            disabled={buttonToggleState}
+          >
+            로그인
+          </button>
+          <button
+            className="signUpButton"
+            onClick={signUpButtonOnClickFunction}
+          >
+            회원가입
+          </button>
+        </form>
         <a className="FindPw" href="https://www.youtube.com/">
           비밀번호를 잊으셨나요?
         </a>
